8e9d5f05b39b79ebee81741cf0ca27a18d0671bf
Egor Sazanovich <egor@prodev.io>
2015-06-21 21:31:07 +0300

    Download it!
    Download function added.
    Download file/directory recursively functions added
    Directory listing function extended

--- curlftp.php
+++ curlftp.php
@@ -25,12 +25,18 @@ class Curlftp {
         'debug' => true,
         'log' => "",
         'timeout_connection' => 10,
+        'threads' => 5,
         'timeout_request' => 10,
         'active_mode' => false
     );
+    //  download queue 
+    private $queue = array();
     //  handlers
     public $connection = false;
     public $curl = false;
+    public $threads = array();
+    public $multiThreads = array();
+    public $local = "/";
 
     //  params must be an array with configuration. 
     //  at least 'host','user','password' to override default values
@@ -49,30 +55,161 @@ class Curlftp {
         } else {
             $this->configuration[$key] = $value;
         }
-        $this->configuration['ftp'] = "ftp://" . $this->configuration['user'] . ":" . $this->configuration['password'] . '@' . $this->configuration['host'] . '/';
+        $this->configuration['ftp'] = "ftp://" . $this->configuration['user'] . ":" . $this->configuration['password'] . '@' . $this->configuration['host'] . ":" . $this->configuration['port'] . '/';
     }
 
     public function _get($key) {
         return $this->configuration[$key];
     }
 
+    /* Helpers */
+
+    function _correctPath($path) {
+        if (mb_substr($path, 0, 1) == "/")
+            $path = mb_substr($path, 1, mb_strlen($path));
+        return $path;
+    }
+
+    function _getCHMOD($string) {
+        $result = array();
+        //  remove sticky bit
+        foreach (str_split(substr($string, 1, strlen($string) - 1), 3) as $chunk) {
+            $id = count($result);
+            $access = 0;
+            $mask = str_split($chunk, 1);
+            if ($mask[0] == "r")
+                $access = $access + 4;
+            if ($mask[1] == "w")
+                $access = $access + 2;
+            if ($mask[2] == "x")
+                $access = $access + 1;
+            $result[$id] = $access;
+        }
+        return (int) implode('', $result);
+    }
+
+    function _isDir($path) {
+        if (mb_substr($path, mb_strlen($path) - 1, 1) == "/")
+            return true;
+        return false;
+    }
+
+    function _prepareLocal($directories = array(), $base = '/') {
+        if (!empty($directories) && isset($directories['content']) && !empty($directories['content'])) {
+            foreach ($directories['content'] as $folder => $content) {
+                //d($content);
+                if ($content['type'] == 'dir') {
+                    if (!file_exists($base . $content['name'] . '/') || !is_dir($base . $content['name'])) {
+                        mkdir($base . $content['name'] . '/', 0777);
+                    }
+                    if ($content['content'])
+                        $this->_prepareLocal($content, $base . $content['name'] . '/');
+                }
+            }
+        }
+    }
+
+    function _addDirToQueue($directory = array(), $base = '/') {
+        if (empty($directory['content']))
+            return true;
+        foreach ($directory['content'] as $name => $data) {
+            if ($data['type'] == 'file') {
+                $this->queue[] = array(
+                    'remote' => $data['path'],
+                    'local' => $base . $data['name'],
+                    'chmod' => $data['chmod']
+                );
+            } else {
+                $this->_addDirToQueue($data, $base . $data['name'] . '/');
+            }
+        }
+    }
+
+    function _processQueue() {
+        if (empty($this->queue))
+            return true;
+        $this->threads = array();
+        $x = 0;
+        foreach ($this->queue as $qid => $request) {
+            if ($x >= $this->configuration['threads'])
+                break;
+            $id = count($this->threads);
+            $this->threads[count($this->threads)] = curl_init();
+            $this->_ftp_connect($this->threads[$id], $request['remote'], true);
+            $file = fopen($this->local . $request['local'], 'w');
+            curl_setopt($this->threads[$id], CURLOPT_URL, $this->configuration['ftp'] . $request['remote']);
+            curl_setopt($this->threads[$id], CURLOPT_BINARYTRANSFER, TRUE);
+            curl_setopt($this->threads[$id], CURLOPT_FILE, $file);
+            unset($this->queue[$qid]);
+            $x++;
+        }
+        $this->multiThreads = curl_multi_init();
+        foreach ($this->threads as $thread) {
+            curl_multi_add_handle($this->multiThreads, $thread);
+        }
+        $active = null;
+        //   process them
+        do {
+            $mrc = curl_multi_exec($this->multiThreads, $active);
+        } while ($mrc == CURLM_CALL_MULTI_PERFORM);
+        while ($active) {
+            if (curl_multi_select($this->multiThreads) != -1) {
+                do {
+                    $mrc = curl_multi_exec($this->multiThreads, $active);
+                } while ($mrc == CURLM_CALL_MULTI_PERFORM);
+            }
+        }
+        // close 
+        foreach ($this->threads as $thread) {
+            curl_multi_remove_handle($this->multiThreads, $thread);
+        }
+        curl_multi_close($this->multiThreads);
+        if (!empty($this->queue))
+            $this->_processQueue();
+        return true;
+    }
+
+    function _setChmod($directory, $path = '/') {
+        if ($directory['content']) {
+            foreach ($directory['content'] as $id => $xdata) {
+                switch ($xdata['type']) {
+                    case 'file':
+                        chmod($this->local . $path . $xdata['name'], octdec("0" . $xdata['chmod']));
+                        break;
+                    case 'dir':
+                        chmod($this->local . $path . $xdata['name'] . "/", octdec("0" . $xdata['chmod']));
+                        $this->_setChmod($xdata, ($path == '/' || $path == '' ? "" : $path) . $xdata['name'] . "/");
+                        break;
+                }
+            }
+        }
+    }
+
     /* cURL function */
 
-    public function _ftp_connect() {
+    public function _ftp_connect($threadid = false, $path = false, $initOnly = false) {
 
-        curl_setopt($this->curl, CURLOPT_URL, $this->configuration['ftp']);
-        $this->configuration['log'] = fopen('php://temp', 'rw+');
-        curl_setopt($this->curl, CURLOPT_VERBOSE, 1);
-        curl_setopt($this->curl, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($this->curl, CURLOPT_CONNECTTIMEOUT, $this->configuration['timeout_connection']);
-        curl_setopt($this->curl, CURLOPT_TIMEOUT, $this->configuration['timeout_request']);
-        curl_setopt($this->curl, CURLOPT_STDERR, $this->configuration['log']);
-        curl_setopt($this->curl, CURLINFO_HEADER_OUT, $this->configuration['debug']);
-        if ($this->configuration['ssl'] == false) {
-            curl_setopt($this->curl, CURLOPT_SSL_VERIFYHOST, FALSE);
-            curl_setopt($this->curl, CURLOPT_SSL_VERIFYPEER, FALSE);
+        if ($threadid == false) {
+            $thread = $this->curl;
+        } else {
+            $thread = $threadid;
         }
-        $response = curl_exec($this->curl);
+
+        curl_setopt($thread, CURLOPT_URL, $path ? $path : $this->configuration['ftp']);
+        $this->configuration['log'] = fopen('php://temp', 'rw+');
+        curl_setopt($thread, CURLOPT_VERBOSE, 1);
+        curl_setopt($thread, CURLOPT_RETURNTRANSFER, TRUE);
+        curl_setopt($thread, CURLOPT_CONNECTTIMEOUT, $this->configuration['timeout_connection']);
+        curl_setopt($thread, CURLOPT_TIMEOUT, $this->configuration['timeout_request']);
+        curl_setopt($thread, CURLOPT_STDERR, $this->configuration['log']);
+        curl_setopt($thread, CURLINFO_HEADER_OUT, $this->configuration['debug']);
+        if ($this->configuration['ssl'] == false) {
+            curl_setopt($thread, CURLOPT_SSL_VERIFYHOST, FALSE);
+            curl_setopt($thread, CURLOPT_SSL_VERIFYPEER, FALSE);
+        }
+        if ($initOnly)
+            return true;
+        $response = curl_exec($thread);
         if ($response !== false)
             return true;
         $this->connection = true;
@@ -100,7 +237,7 @@ class Curlftp {
         curl_close($this->curl);
     }
 
-    public function parseDirList($listing, $order) {
+    public function parseDirList($listing, $order, $path = '/') {
         $files = array();
         $folders = array();
         foreach (array_filter(explode(PHP_EOL, $listing)) as $id => $line) {
@@ -108,13 +245,26 @@ class Curlftp {
             end($items);
             $key = key($items);
             $name = $items[$key];
-            $size = $items[array_keys($items)[4]];
+            $size = (int) $items[array_keys($items)[4]];
             if (mb_substr($items[0], 0, 1) == "d") {
                 //  directory
-                $folders[$name] = FALSE;
+                $folders[$name] = array(
+                    'name' => $name,
+                    'path' => '/' . $path . $name . '/',
+                    'size' => FALSE,
+                    'type' => 'dir',
+                    'chmod' => $this->_getCHMOD($items[0]),
+                    'content' => false
+                );
             } else {
                 //  file
-                $files[$name] = $size;
+                $files[$name] = array(
+                    'name' => $name,
+                    'path' => '/' . $path . $name,
+                    'size' => $size,
+                    'type' => 'file',
+                    'chmod' => $this->_getCHMOD($items[0])
+                );
             }
         }
         $result = array();
@@ -139,29 +289,133 @@ class Curlftp {
 
     /* Actions */
 
-    //  Directory Listing
-    //  ---
-    //  Return directory list if directory exist. FALSE if not.
-    //  Accepts path to folder as first argument. With or without starting slash
-    //  Accepts result sorting as second argument(string). Available sort types:
-    //  
-    //  default - order by Name ASC, folders first, files after
-    //  name - order by Name ASC, mixing folders and files
-    //  rname - order by Name DESC, mixing folders and files
-    public function dir($path = '/web/', $order = 'default') {
+//  Directory Listing
+//  ---
+//  Return directory list if directory exist. FALSE if not.
+//  Accepts path to folder as first argument. With or without starting slash
+//  Accepts result sorting as second argument(string). Available sort types:
+//  
+//  default - order by Name ASC, folders first, files after
+//  name - order by Name ASC, mixing folders and files
+//  rname - order by Name DESC, mixing folders and files
+    public function dir($path = '/', $order = 'default') {
         //  correct path
-        if (mb_substr($path, 0, 1) == "/")
-            $path = mb_substr($path, 1, mb_strlen($path));
+        $path = $this->_correctPath($path);
         //  check connection
         if ($this->connect() || $this->connect(true)) {
             curl_setopt($this->curl, CURLOPT_URL, $this->configuration['ftp'] . $path);
             $response = $this->_ftp_query("LIST", TRUE);
             if (!$response)
                 return false;
-            return $this->parseDirList($response, $order);
+            return array(
+                'type' => 'dir',
+                'chmod' => false,
+                'content' => $this->parseDirList($response, $order, $path));
         } else {
             return false;
         }
+    }
+
+    public function dirTree($path = '/', $dirinfo = false) {
+        $result = array();
+        $dir = $this->dir($path);
+        if ($dir == false)
+            return array(
+                'name' => $dirinfo ? $dirinfo['name'] : false,
+                'path' => $path,
+                'type' => 'dir',
+                'chmod' => $dirinfo ? $dirinfo['chmod'] : false,
+                'content' => false
+            );
+        $result = array(
+            'name' => $dirinfo ? $dirinfo['name'] : false,
+            'path' => $path,
+            'type' => $dir['type'],
+            'chmod' => $dirinfo ? $dirinfo['chmod'] : false,
+            'content' => false
+        );
+        foreach ($dir['content'] as $id => $file) {
+            if ($file['type'] == 'file')
+                $tmp[] = $file;
+            if ($file['type'] == 'dir')
+                $tmp[] = $this->dirTree($file['path'], $file);
+        }
+        $result['content'] = $tmp;
+        return $result;
+    }
+
+    //  Download single file from $remote server to $local host
+    public function download_file($remote, $local) {
+        if ($this->connect() || $this->connect(true)) {
+            $file = fopen($local, 'w');
+            curl_setopt($this->curl, CURLOPT_URL, $this->configuration['ftp'] . $remote);
+            curl_setopt($this->curl, CURLOPT_BINARYTRANSFER, TRUE);
+            curl_setopt($this->curl, CURLOPT_FILE, $file);
+            $response = curl_exec($this->curl);
+            fclose($file);
+            if ($response == false)
+                unlink($local);
+            return $response;
+        } else {
+            return false;
+        }
+    }
+
+    // Download file/files or directories, depends on $remote
+    // $remote can be an array of files/directories or single file/directory
+    // $local is a path to save files/directories from $remote
+    public function download($remote, $local) {
+        if (is_array($remote)) {
+            foreach ($remote as $rmt) {
+                if ($this->_isDir($rmt)) {
+                    $directory = $this->dirTree($rmt);
+                    if (!empty($directory)) {
+                        $this->_prepareLocal($directory, $local);
+                        $this->_addDirToQueue($directory);
+                    }
+                } else {
+                    if (is_dir($local)) {
+                        $rpath = explode("/", $remote);
+                        $local = $local . $rpath[count($rpath) - 1];
+                    }
+                    $this->queue[] = array(
+                        'local' => $local,
+                        'remote' => $remote,
+                        'chmod' => 644
+                    );
+                }
+            }
+        } else {
+            if ($this->_isDir($remote)) {
+                $directory = $this->dirTree($remote);
+                if (!empty($directory)) {
+                    $this->_prepareLocal($directory, $local);
+                    $this->_addDirToQueue($directory);
+                }
+            } else {
+                if (is_dir($local)) {
+                    $rpath = explode("/", $remote);
+                    $local = $local . $rpath[count($rpath) - 1];
+                }
+                $this->queue[] = array(
+                    'local' => $local,
+                    'remote' => $remote,
+                    'chmod' => 644
+                );
+            }
+        }
+        if (!is_writable(dirname($local))) {
+            die('Directory ' . dirname($local) . ' must be writeable');
+        }
+        $this->local = $local;
+        if (!empty($this->queue)) {
+            $this->_processQueue();
+            //   set chmod to downloaded files and directories
+            if ($directory) {
+                $this->_setChmod($directory, '');
+            }
+        }
+        return true;
     }
 
 }
